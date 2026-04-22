cc16526da30d4740e099b01c474f14cf54a9e0ea
BogdanATA <batameev@gmail.com>
2026-04-22 11:29:20 -0400

    - added logic to getInvetory method. now it reads inventory.csv file and creates Product object from it

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,8 +1,14 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class SearchInventory {
+
+    private static final String INV_FILE_NAME = "inventory.csv";
+
     public static void main(String[] args) {
         ArrayList<Product> inventory = getInventory();
 
@@ -16,7 +22,25 @@ public class SearchInventory {
         ArrayList<Product> inventory = new ArrayList<>();
 
         // Write your code here
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(INV_FILE_NAME));
+            String input;
 
+            while ((input = bufferedReader.readLine()) != null) {
+                String[] tokens = input.split("\\|"); // break line into parts everytime it reads "|"
+
+                int id = Integer.parseInt(tokens[0]);
+                String name = tokens[1];
+                double price = Double.parseDouble(tokens[2]);
+                // create new Product object
+                Product product = new Product(id, name, price);
+                inventory.add(product);
+            }
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            System.err.println("Error reading inventory file.");
+        }
         return inventory;
     }
 }
